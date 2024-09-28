fee6b0b8419195fdc1e559fda45b6edccab99ea5
Brian Yeap <bryeap@u81z01s03.42kl.edu.my>
2024-09-28 20:14:12 +0800

    added total

--- main/ending.c
+++ main/ending.c
@@ -6,7 +6,7 @@
 /*   By: bryeap <bryeap@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 20:10:25 by brian             #+#    #+#             */
-/*   Updated: 2024/09/27 21:33:39 by bryeap           ###   ########.fr       */
+/*   Updated: 2024/09/28 20:13:48 by bryeap           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ void	win_screen(t_map *map)
 	mlx_string_put(map->mlx, map->wnd,
 		map->x / 2 * IMG_PXL, map->y / 2 * IMG_PXL, 0xFFFA9E, "YOU WON!");
 	ft_putstr_fd("\n\x1b[32;01m", 1);
-	ft_putstr_fd("YOU WON !!!", 1);
+	ft_putstr_fd("YOU WON !!!\n", 1);
+	ft_putstr_fd("\nTotal Moves: ", 1);
+	ft_putstr_fd(ft_itoa(map->moves), 1);
 	ft_putstr_fd("\n\x1b[0m", 1);
 }
